7f42af14416b1f03825a80bf1b02db577ca2f2db
Jacob Sampson <jacob.samps@gmail.com>
2020-06-23 17:47:04 -0500

    Initial working POC

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.scss';
 import Footer from './footer/Footer';
 import Folderol from './folderol/Folderol';
 import Form from './form/Form';
 import RepoOverview from './repo-overview/RepoOverview';
+import GeneratedWords from './generated-words/GeneratedWords';
 
 const CLASS = 'app';
 function App() {
+  const [words, updateWords] = useState<{ word: string, originalWord: string, languageCode: string }[]>([]);
+
+  const addWords = (newWords: { word: string, originalWord: string, languageCode: string }[]) => {
+    const otherWords: string[] = [];
+    updateWords([...words, ...newWords].filter(word => {
+      if (otherWords.some(otherWord => otherWord === word.word)) {
+        return false;
+      }
+      otherWords.push(word.word);
+      return true;
+    }));
+  }
+  
   return (
     <div className={CLASS}>
-      <RepoOverview></RepoOverview>
+      <RepoOverview />
       <main className={CLASS + '__main'}>
-        <Form />
+        <Form addWords={addWords}/>
         <div className={`${CLASS}__folderol`}>
           <Folderol />
         </div>
+        <GeneratedWords words={words} />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
